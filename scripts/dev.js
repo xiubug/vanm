be5ff109221fdbb0da4803828b3283a7b420e36a
魏昌华 <weichanghua@linctex.com>
2019-11-16 13:26:27 +0800

    :art: update project dir

---
+++ scripts/dev.js
@@ -0,0 +1,14 @@
+const spawn = require('cross-spawn')
+
+let target = process.argv[2]
+const alias = {
+  docs: 'vuetifyjs.com',
+  kitchen: '@vuetify/kitchen',
+}
+target = alias[target] || target
+
+if (!target) {
+  spawn('yarn', ['lerna', 'run', 'dev', '--scope', 'vanm', '--stream'], { stdio: 'inherit' })
+} else {
+  spawn('yarn', ['lerna', 'run', 'dev', '--scope', target, '--stream'], { stdio: 'inherit' })
+}
